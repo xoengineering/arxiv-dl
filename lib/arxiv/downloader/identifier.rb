19221238c013ca88086d5292a0558369965b3f8a
Shane Becker <veganstraightedge@gmail.com>
2026-05-01 21:47:21 -0700

    Add Identifier version parsing; relax RSpec multi-expectation cops

--- lib/arxiv/downloader/identifier.rb
+++ lib/arxiv/downloader/identifier.rb
@@ -1,10 +1,15 @@
 module Arxiv
   module Downloader
     class Identifier
-      attr_reader :id
+      MODERN_ID = /\A(\d{4}\.\d{4,5})(?:v(\d+))?\z/
+
+      attr_reader :id, :version
 
       def initialize input
-        @id = input
+        match = MODERN_ID.match input
+
+        @id      = match[1]
+        @version = match[2]&.to_i
       end
     end
   end
--- spec/arxiv/downloader/identifier_spec.rb
+++ spec/arxiv/downloader/identifier_spec.rb
@@ -2,6 +2,14 @@ RSpec.describe Arxiv::Downloader::Identifier do
   it 'parses a bare modern arxiv ID' do
     identifier = described_class.new '1512.03385'
 
-    expect(identifier.id).to eq '1512.03385'
+    expect(identifier.id).to      eq '1512.03385'
+    expect(identifier.version).to be_nil
+  end
+
+  it 'parses a versioned bare modern arxiv ID' do
+    identifier = described_class.new '1512.03385v2'
+
+    expect(identifier.id).to      eq '1512.03385'
+    expect(identifier.version).to eq 2
   end
 end
