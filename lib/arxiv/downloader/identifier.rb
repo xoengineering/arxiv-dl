d20ffa6391873bfc08bcd3c5ceff2d9649755a1d
Shane Becker <veganstraightedge@gmail.com>
2026-05-01 21:47:44 -0700

    Add Identifier arXiv: prefix parsing

--- lib/arxiv/downloader/identifier.rb
+++ lib/arxiv/downloader/identifier.rb
@@ -2,11 +2,12 @@ module Arxiv
   module Downloader
     class Identifier
       MODERN_ID = /\A(\d{4}\.\d{4,5})(?:v(\d+))?\z/
+      PREFIX    = /\AarXiv:/i
 
       attr_reader :id, :version
 
       def initialize input
-        match = MODERN_ID.match input
+        match = MODERN_ID.match input.sub(PREFIX, '')
 
         @id      = match[1]
         @version = match[2]&.to_i
--- spec/arxiv/downloader/identifier_spec.rb
+++ spec/arxiv/downloader/identifier_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe Arxiv::Downloader::Identifier do
     expect(identifier.id).to      eq '1512.03385'
     expect(identifier.version).to eq 2
   end
+
+  it 'parses a prefixed arxiv ID' do
+    identifier = described_class.new 'arXiv:1512.03385'
+
+    expect(identifier.id).to      eq '1512.03385'
+    expect(identifier.version).to be_nil
+  end
+
+  it 'parses a prefixed and versioned arxiv ID' do
+    identifier = described_class.new 'arXiv:1512.03385v2'
+
+    expect(identifier.id).to      eq '1512.03385'
+    expect(identifier.version).to eq 2
+  end
 end
