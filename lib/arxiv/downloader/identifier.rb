c41020ff64bdf8a67ddd2af68c7dbb408d834feb
Shane Becker <veganstraightedge@gmail.com>
2026-05-01 21:48:22 -0700

    Add Identifier arxiv URL parsing (abs/pdf/html)

--- lib/arxiv/downloader/identifier.rb
+++ lib/arxiv/downloader/identifier.rb
@@ -1,16 +1,27 @@
 module Arxiv
   module Downloader
     class Identifier
-      MODERN_ID = /\A(\d{4}\.\d{4,5})(?:v(\d+))?\z/
-      PREFIX    = /\AarXiv:/i
+      MODERN_ID  = /\A(\d{4}\.\d{4,5})(?:v(\d+))?\z/
+      PREFIX     = /\AarXiv:/i
+      URL_PREFIX = %r{\Ahttps?://arxiv\.org/(?:abs|pdf|html)/}
+      PDF_SUFFIX = /\.pdf\z/
 
       attr_reader :id, :version
 
       def initialize input
-        match = MODERN_ID.match input.sub(PREFIX, '')
+        match = MODERN_ID.match normalize(input)
 
         @id      = match[1]
         @version = match[2]&.to_i
+      end
+
+      private
+
+      def normalize input
+        input
+          .sub(URL_PREFIX, '')
+          .sub(PDF_SUFFIX, '')
+          .sub(PREFIX, '')
       end
     end
   end
--- spec/arxiv/downloader/identifier_spec.rb
+++ spec/arxiv/downloader/identifier_spec.rb
@@ -26,4 +26,46 @@ RSpec.describe Arxiv::Downloader::Identifier do
     expect(identifier.id).to      eq '1512.03385'
     expect(identifier.version).to eq 2
   end
+
+  it 'parses an abstract page URL' do
+    identifier = described_class.new 'https://arxiv.org/abs/1512.03385'
+
+    expect(identifier.id).to      eq '1512.03385'
+    expect(identifier.version).to be_nil
+  end
+
+  it 'parses a versioned abstract page URL' do
+    identifier = described_class.new 'https://arxiv.org/abs/1512.03385v2'
+
+    expect(identifier.id).to      eq '1512.03385'
+    expect(identifier.version).to eq 2
+  end
+
+  it 'parses a PDF URL' do
+    identifier = described_class.new 'https://arxiv.org/pdf/1512.03385.pdf'
+
+    expect(identifier.id).to      eq '1512.03385'
+    expect(identifier.version).to be_nil
+  end
+
+  it 'parses a versioned PDF URL' do
+    identifier = described_class.new 'https://arxiv.org/pdf/1512.03385v2.pdf'
+
+    expect(identifier.id).to      eq '1512.03385'
+    expect(identifier.version).to eq 2
+  end
+
+  it 'parses an HTML version URL' do
+    identifier = described_class.new 'https://arxiv.org/html/2506.15442'
+
+    expect(identifier.id).to      eq '2506.15442'
+    expect(identifier.version).to be_nil
+  end
+
+  it 'parses a versioned HTML version URL' do
+    identifier = described_class.new 'https://arxiv.org/html/2506.15442v1'
+
+    expect(identifier.id).to      eq '2506.15442'
+    expect(identifier.version).to eq 1
+  end
 end
