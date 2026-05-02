a81b847b68e6aa5ab793604cea69dd66c5780968
Shane Becker <veganstraightedge@gmail.com>
2026-05-01 21:53:11 -0700

    Add Slug word-boundary truncation at 80 chars

--- lib/arxiv/downloader/slug.rb
+++ lib/arxiv/downloader/slug.rb
@@ -3,12 +3,30 @@ require 'stringex'
 module Arxiv
   module Downloader
     class Slug
+      MAX_LENGTH = 80
+
       def initialize title
         @title = title
       end
 
       def to_s
-        @title.to_url
+        truncate @title.to_url
+      end
+
+      private
+
+      def truncate slug
+        return slug if slug.length <= MAX_LENGTH
+
+        words = slug.split '-'
+        result = +''
+        words.each do |word|
+          break if result.length + 1 + word.length > MAX_LENGTH
+
+          result << '-' unless result.empty?
+          result << word
+        end
+        result
       end
     end
   end
--- spec/arxiv/downloader/slug_spec.rb
+++ spec/arxiv/downloader/slug_spec.rb
@@ -3,4 +3,13 @@ RSpec.describe Arxiv::Downloader::Slug do
     expect(described_class.new('Deep Residual Learning for Image Recognition').to_s)
       .to eq 'deep-residual-learning-for-image-recognition'
   end
+
+  it 'truncates a long title to 80 chars at a word boundary' do
+    title = 'Deep Residual Learning for Image Recognition with Convolutional Neural Networks Applied to Medical Diagnosis'
+    slug  = described_class.new(title).to_s
+
+    expect(slug.length).to be <= 80
+    expect(slug).not_to    end_with '-'
+    expect(slug).to        eq 'deep-residual-learning-for-image-recognition-with-convolutional-neural-networks'
+  end
 end
